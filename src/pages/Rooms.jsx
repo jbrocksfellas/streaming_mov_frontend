240e7b7a5b64a1cee0a4b2213ed91cd9a05b467e
Gaurav Singh <rajputgaurav4268@gmail.com>
2023-02-04 05:17:46 +0530

    removed wrtc

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -36,104 +36,6 @@ export default function Rooms() {
     form.resetFields();
   };
 
-  function callUser(userID) {
-    peerRef.current = createPeer(userID);
-    userStream.current.getTracks().forEach((track) => senders.current.push(peerRef.current.addTrack(track, userStream.current)));
-  }
-
-  function createPeer(userID) {
-    const peer = new RTCPeerConnection({
-      iceServers: [
-        {
-          urls: "stun:stun.stunprotocol.org",
-        },
-        {
-          urls: "turn:numb.viagenie.ca",
-          credential: "muazkh",
-          username: "webrtc@live.com",
-        },
-      ],
-    });
-
-    peer.onicecandidate = handleICECandidateEvent;
-    peer.ontrack = handleTrackEvent;
-    peer.onnegotiationneeded = () => handleNegotiationNeededEvent(userID);
-
-    return peer;
-  }
-
-  function handleNegotiationNeededEvent(userID) {
-    peerRef.current
-      .createOffer()
-      .then((offer) => {
-        return peerRef.current.setLocalDescription(offer);
-      })
-      .then(() => {
-        const payload = {
-          target: userID,
-          caller: socketRef.current.id,
-          sdp: peerRef.current.localDescription,
-        };
-        socketRef.current.emit("offer", payload);
-      })
-      .catch((e) => console.log(e));
-  }
-
-  function handleRecieveCall(incoming) {
-    peerRef.current = createPeer();
-    const desc = new RTCSessionDescription(incoming.sdp);
-    peerRef.current
-      .setRemoteDescription(desc)
-      .then(() => {
-        userStream.current.getTracks().forEach((track) => peerRef.current.addTrack(track, userStream.current));
-      })
-      .then(() => {
-        return peerRef.current.createAnswer();
-      })
-      .then((answer) => {
-        return peerRef.current.setLocalDescription(answer);
-      })
-      .then(() => {
-        const payload = {
-          target: incoming.caller,
-          caller: socketRef.current.id,
-          sdp: peerRef.current.localDescription,
-        };
-        socketRef.current.emit("answer", payload);
-      });
-  }
-
-  function handleAnswer(message) {
-    const desc = new RTCSessionDescription(message.sdp);
-    peerRef.current.setRemoteDescription(desc).catch((e) => console.log(e));
-  }
-
-  function handleICECandidateEvent(e) {
-    if (e.candidate) {
-      const payload = {
-        target: otherUser.current,
-        candidate: e.candidate,
-      };
-      socketRef.current.emit("ice-candidate", payload);
-    }
-  }
-
-  function handleNewICECandidateMsg(incoming) {
-    const candidate = new RTCIceCandidate(incoming);
-
-    peerRef.current.addIceCandidate(candidate).catch((e) => console.log(e));
-  }
-
-  function handleTrackEvent(e) {
-    partnerVideo.current.srcObject = e.streams[0];
-  }
-
-  function shareScreen() {
-    navigator.mediaDevices.getDisplayMedia({ cursor: true }).then((stream) => {
-      const screenTrack = stream.getTracks()[0];
-    });
-  }
-
   useEffect(() => {
     if (username && id) {
       sendJoinRequest({ id, username });
@@ -141,33 +43,6 @@ export default function Rooms() {
 
     return () => {};
   }, []);
-
-  useEffect(() => {
-    if (hasJoined) {
-      navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then((stream) => {
-        userVideo.current.srcObject = stream;
-        userStream.current = stream;
-
-        // socketRef.current = io.connect("/");
-        // socketRef.current.emit("join room", props.match.params.roomID);
-
-        // socketRef.current.on("other user", (userID) => {
-        //   callUser(userID);
-        //   otherUser.current = userID;
-        // });
-
-        // socketRef.current.on("user joined", (userID) => {
-        //   otherUser.current = userID;
-        // });
-
-        socket.on("offer", handleRecieveCall);
-
-        socket.on("answer", handleAnswer);
-
-        socket.on("ice-candidate", handleNewICECandidateMsg);
-      });
-    }
-  }, [hasJoined]);
 
   return (
     <div>
@@ -200,11 +75,11 @@ export default function Rooms() {
             })}
           </div>
           <div>
-            <video controls style={{ height: 500, width: 500 }} autoPlay ref={userVideo} />
+            {/* <video controls style={{ height: 500, width: 500 }} autoPlay ref={userVideo} />
             <video controls style={{ height: 500, width: 500 }} autoPlay ref={partnerVideo} />
-            <button onClick={shareScreen}>Screen Share</button>
+            <button onClick={shareScreen}>Screen Share</button> */}
           </div>
-          {/* <ReactPlayer style={{ width: "100%", background: "blue" }} url={mediaStream} ref={ref} playing={playing} /> */}
+          <ReactPlayer style={{ width: "100%", background: "blue" }} url={mediaStream} ref={ref} playing={playing} />
           <div>
             <div style={{ marginTop: "10px" }}>
               {messages.map((msg, i) => {
